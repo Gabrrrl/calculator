fa65a6dd8f524823aa57739963027373b4d12351
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-10 22:25:19 -0700

    Added calculator ui/ux, and some basic arithmetic functions inside of javascript file

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// arithmetic functions
+const add = function (a, b) {
+    return a + b
+};
+
+const subtract = function (a, b) {
+    return a - b
+};
+
+const sum = function (nums) {
+    let sumAll = nums.reduce((num1, num2) => num1 + num2, 0)
+    return sumAll
+};
+
+const multiply = function (nums) {
+    let multiplyAll = nums.reduce((num1, num2) => num1 * num2)
+    return multiplyAll
+
+};
+
+const power = function (a, b) {
+    return a ** b
+};
+
+const factorial = function (num) {
+    let answer = 1
+    if (num == 0 || num == 1) {
+        return answer
+    } else {
+        for (let i = num; i >= 1; i--) {
+            answer = answer * i
+        }
+        return answer
+    }
+};
+
